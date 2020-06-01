e62195f3eefdfa11c33159f318832ede36ab5fea
tomaz-suller <tomaz.suller@usp.br>
2020-06-01 12:25:57 -0300

    Adapt for storage of two data points per sensor

--- codigo/codigo.ino
+++ codigo/codigo.ino
@@ -8,30 +8,33 @@ typedef struct{
   int limIrrig; //Limite de umidade do solo para o qual a planta deve ser irrigada
 } ESPECIE_TIPO;
 
-//Sensores
+/* Sensores */
 #define ldrPin A0
-#define higrometro1Pin A4
-#define higrometro2Pin A5
+#define higrometroPin A4
 #define dhtPin 13
 #define sensorAguaPin A3
 
 DHT dht(dhtPin, DHT11);
 
-//Entrada
+/* Entrada */
 #define b1Pin 1
 #define b2Pin 2
 #define b3Pin 3
 
-//Saida
+/* Saida */
 #define ledPin 4
 LiquidCrystal lcd(10, 9, 8, 7, 6, 5);
 
-//Variáveis globais
+/* Variaveis globais */
 #define nEspecies 2
+
+//Arrays para armazenar valores dos sensores
+int b1[2], b2[2], b3[2], lum[2], hsolo[2];
+float h_ar[2], temp[2];
 
 volatile int scrollstate = -1;
 String texto = "Temperatura: °C Hum Rel do Ar: % Hum do Solo: ";
-#define tamanho_texto 56 //Ainda precisa contar o numero de caracteres da umidade do solo
+#define tamanho_texto 60 //Ainda precisa contar o numero de caracteres da umidade do solo
 
 //Para as partes do programa que dependem da especie escolhida
 volatile int especie = 0;
@@ -41,24 +44,34 @@ ESPECIE_TIPO especies[nEspecies] = {{"Cebolinha", 100}, {"Manjericao", 200}}; //
 
 
 //Le as informacoes dos sensores
-void leSensores(int* b_especie, int* b_irrigacao, int* b_scroll, int* lum, int* hsolo1, int* hsolo2, int* nagua, float* h_ar, float* temp){
+void leSensores(int* b_especie, int* b_irrigacao, int* b_scroll, int* lum, int* hsolo, float* h_ar, float* temp){
+  
+  //Armazena ultimo valor do sensor
+  b_especie[0] = b_especie[1]
+  b_irrigacao[0] = b_irrigacao[1] 
+  b_scroll[0] = b_scroll[1]
+
+  lum[0] = lum[1]
+  hsolo[0] = hsolo[1]
+
+  h_ar[0] = h_ar[1]
+  temp[0] = temp[1]
+  
   //Definicao das entradas dos botoes**************************************
-  *b_especie = digitalRead(b1Pin);
-  *b_irrigacao = digitalRead(b2Pin);
-  *b_scroll = digitalRead(b3Pin);
+  b_especie[1] = digitalRead(b1Pin);
+  b_irrigacao[1] = digitalRead(b2Pin);
+  b_scroll[1] = digitalRead(b3Pin);
 
   //Armazenamento das entradas dos sensores********************************
-  *lum = analogRead(ldrPin);
-  *hsolo1 = analogRead(higrometro1Pin);
-  *hsolo2 = analogRead(higrometro2Pin);
-  *nagua = analogRead(sensorAguaPin); //Precisa ser calibrado
+  lum[1] = analogRead(ldrPin);
+  hsolo[1] = analogRead(higrometroPin);
 
-  *h_ar = dht.readHumidity();
-  *temp = dht.readTemperature();
+  h_ar[1] = dht.readHumidity();
+  temp[1] = dht.readTemperature();
 }
 
 //Mostra as informacoes dos sensores no display
-void imprimeSensores(int lum, int hsolo1, int hsolo2, int nagua, float h_ar, float temp){
+void imprimeSensores(int lum, int hsolo, float h_ar, float temp){
   
   lcd.setCursor(1,0);
   lcd.print(especies[especie].nome);
@@ -69,13 +82,13 @@ void imprimeSensores(int lum, int hsolo1, int hsolo2, int nagua, float h_ar, flo
   lcd.print(" °C ");
   //Imprime exatamente 16 caracteres
   if(scrollstate == 1){
-    texto = texto.substring(0,12) + String(temp) + texto.substring(13,30) + String(h_ar) + texto.substring(31,46) + String(hsolo1);
+    texto = texto.substring(0,12) + String(temp) + texto.substring(13,30) + String(h_ar) + texto.substring(31,46) + String(hsolo);
 
     int ini = 0, fim = 16;
 
     for(; fim <= tamanho_texto; ini++, fim++){
       lcd.setCursor(0, 0);
-      lcd.print( message.substring(ini, fim) ); 
+      lcd.print( texto.substring(ini, fim) ); 
       delay(300);
     }
 
@@ -83,18 +96,15 @@ void imprimeSensores(int lum, int hsolo1, int hsolo2, int nagua, float h_ar, flo
 
 }
 
-void irriga(){
-  //Implementar mecanismo de irrigacao  
-}
-
 //Interrupcao que troca o indice da especie
 void trocaDeEspecie(){
   especie++;
-  if(especie>=nEspecies){
-    especie=0;  
+  if(especie >= nEspecies){
+    especie = 0;  
   }  
 }
 
+//Interrupcao que ativa e desliga rolamento dos dados no LCD
 void scroll(){
   scrollstate *= -1;
 }
@@ -107,8 +117,7 @@ void setup() {
 
   //Sensores
   pinMode(ldrPin, INPUT);
-  pinMode(higrometro1Pin, INPUT);
-  pinMode(higrometro2Pin, INPUT);
+  pinMode(higrometroPin, INPUT);
   pinMode(dhtPin, INPUT);
   pinMode(sensorAguaPin, INPUT);
 
@@ -121,24 +130,20 @@ void setup() {
   pinMode(ledPin, OUTPUT);
 
   //Interrupcoes
-  //Troca de especie ligada ao botao 2
   attachInterrupt(0, trocaDeEspecie, FALLING);
   attachInterrupt(1, scroll, RISING);
 }
 
 void loop() {
 
-  int b1, b2, b3, lum, hsolo1, hsolo2, nagua;
-  float h_ar, temp;
-
   /* Nesta funcao, dos botoes:
   b1 e para irrigar manualmente
-  b2 e para mudar de especie
-  LCD provavelmente deve rodar no auto-scroll, ou por interrupcoes no b3;
+  b2 e para mudar de especie por interrupcao
+  b3 e para alterar o estado do rolamento por interrupcao;
   */
-  leSensores(&b1, &b2, &b3, &lum, &hsolo1, &hsolo2, &nagua, &h_ar, &temp);
+  leSensores(b1, b2, b3, lum, hsolo, h_ar, temp);
 
-  imprimeSensores(lum, hsolo1, hsolo2, nagua, h_ar, temp);
+  imprimeSensores(lum[1], hsolo[1], h_ar[1], temp[1]);
 
   //Bypass do b1 (Irrigacao manual) funciona melhor se usar um switch inves de um botao
   while(b1 == HIGH){
